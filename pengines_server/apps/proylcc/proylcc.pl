8fb77a9fd21dc6cfb3294119c00f7fdf62cec194
Matias Wilberger <matias180399@gmail.com>
2022-04-29 18:55:01 -0300

    implementacion sin testeo del caso base calculo de celdas adyacentes (predicado adyacentes), e implementacion parcial del caso recursivo (predicado adyacentesC)

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -51,3 +51,62 @@ encontrarElemento(Lista,X,Elem):-
 cambiarElemento(PosX, Lista, Color, Res) :-
   nth0(PosX, Lista, _, Aux),
   nth0(PosX, Res, Color, Aux).
+
+
+%!
+% Este predicado tiene como objetivo encontrar todos los adyacentes de
+% una posicion dada, que cumplan con la condicion de que tengan el mismo
+% color que la primer posicion encontrada. Este predicado es recursivo,
+% y buscara todas las posiciones que cumplan con la condicion de
+% adyacencia (ver predicado "adyacentes")
+adyacentesC(Grid, Color, PosX, PosY, ListaRes, ListaAdyacentes):-
+   lenght(Grid,LARGO),
+   ((PosX<0;PosY<0;PosX>LARGO;PosY>LARGO),(append([],[],ListaAdyacentes)));
+   (encontrarLista(Grid,PosY,ListaY),
+    encontrarElemento(ListaY,PosX,Elem),
+    ((not(member(ListaRes,[[PosX,PosY]])),
+     not(dif(Color,Elem)),
+      XAux1 is PosX-1,
+      XAux2 is PosX+1,
+      YAux1 is PosY-1,
+      YAux2 is PosY+1,
+     (member(ListaRes,[[XAux1,PosY]]);member(ListaRes,[[XAux2,PosY]]);
+      member(ListaRes,[[PosX,YAux1]]);member(ListaRes,[[PosX,YAux2]])),
+        append(ListaRes,[[PosX,PosY]],LResAux) %falta llamar recursivamente a x-1,x+1,y-1 e y+1
+     );
+        append(ListaRes,[],LResAux)
+   ), 
+   append(,,ListaAdyacentes)).
+
+%!
+%Predicado base para hallar los adyacentes de una posicion dada,
+% este agrega la posicion principal a la lista de adyacentes, y procede
+% a verificar en sus 4 direcciones si tienen algun adyacente.
+% Una celda adyacente es aquella que es vecina (se encuentra a 1
+% posicion en X o Y), y contiene el mismo color que la Celda de la cual
+% se compara adyacencia.
+adyacentes(Grid, Color, PosX, PosY, ListaRes, ListaAdyacentes):-
+    ListaRes = [],
+    encontrarLista(Grid,PosY,ListaY),
+    length(ListaY,LARGO),
+    encontrarElemento(ListaY,PosX,Elem),
+    not(dif(Color,Elem)),
+    append(ListaRes,[[PosX,PosY]],LRes),
+   (dif(PosX, 0), XAux1 is PosX-1, adyacentesC(Grid,Color, XAux1,PosY,LRes,ResAux1)),
+   (dif(PosX, LARGO), XAux2 is PosX+1,
+                            ((ResAux1 = [], adyacentesC(Grid,Color, XAux2,PosY,LRes,ResAux2));
+                            (not(ResAux1 = []),adyacentesC(Grid,Color, XAux2,PosY,ResAux1,ResAux2)))),
+   (dif(PosY, 0),YAux1 is PosY-1,
+                             (not(ResAux2 = []), adyacentesC(Grid,Color, PosX,YAux1,ResAux2,ResAux3));
+                             (not(ResAux1 = []), adyacentesC(Grid,Color, PosX,YAux1,ResAux1,ResAux3));
+                             (adyacentesC(Grid,Color, PosX,YAux1,LRes,ResAux3))),
+   (dif(PosY, LARGO), YAux2 is PosY+1,
+                             (not(ResAux3 = []), adyacentesC(Grid,Color, PosX,YAux2,ResAux3,ResAux4));
+                             (not(ResAux2 = []), adyacentesC(Grid,Color, PosX,YAux2,ResAux2,ResAux4));
+                             (not(ResAux1 = []), adyacentesC(Grid,Color, PosX,YAux2,ResAux1,ResAux4));
+                             (adyacentesC(Grid,Color, PosX,YAux1,LRes,ResAux4))),
+   ((not(ResAux4 = []), append(ResAux4, [], ListaAdyacentes));
+   (not(ResAux3 = []),  append(ResAux3, [], ListaAdyacentes));
+   (not(ResAux2 = []), append(ResAux2, [], ListaAdyacentes));
+   (not(ResAux1 = []), append(ResAux1, [], ListaAdyacentes));
+   append([],[],ListaAdyacentes)).
