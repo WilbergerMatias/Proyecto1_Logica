02295a62bce62d544d0551737dbcc0f9435fac9a
Franco Feuilles <43554508+NotFukks@users.noreply.github.com>
2022-05-05 18:15:25 -0300

    seleccionar solucionado.

--- src/Board.js
+++ src/Board.js
@@ -13,7 +13,7 @@ class Board extends React.Component {
                             key={i + "." + j}
                             onClick={this.props.onOriginSelected && (() =>
                                 this.props.onOriginSelected(j,i))}
-                            className={this.props.origin[0] === i && this.props.origin[1] === j ? "origin" : undefined}
+                            className={this.props.origin[0] === j && this.props.origin[1] === i ? "origin" : undefined}
                         />
                     )
                 )}
